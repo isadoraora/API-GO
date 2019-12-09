ab601d69db85bfe806bd9277fc4a707dc04463c9
isadoraora <isa.mdeo@gmail.com>
2019-12-09 02:19:21 -0200

    Treinando

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,3 +1,13 @@
 package datastore
 
-import 
+import "github.com/isadoraora/API-GO/loader"
+
+type BookStore interface {
+	Initialize()
+	SearchAuthor(author string, ratingOver, ratingBelow float64, limit, skip int) *[]*loader.BookData
+	SearchBook(bookName string, ratingOver, ratingBelow float64, limit, skip int) *[]*loader.BookData
+	SearchISBN(isbn string) *loader.BookData
+	CreateBook(book *loader.BookData) bool
+	DeleteBook(isbn string) bool
+	UpdateBook(isbn string, book *loader.BookData) bool
+}
